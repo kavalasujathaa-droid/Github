6074cd7ac61ee64382800f72bc05f759e60a9d31
kavalasujathaa-droid <kavalasujathaa@gmail.com>
2026-06-20 10:21:23 +0530

    Update app.js

--- github_deploy/frontend/js/app.js
+++ github_deploy/frontend/js/app.js
@@ -77,7 +77,7 @@ async function handleRegister(){
 
         const response =
         await fetch(
-            "http://127.0.0.1:5000/register",
+          "https://royal-bank-system.onrender.com/register",
             {
                 method:"POST",
                 headers:{
